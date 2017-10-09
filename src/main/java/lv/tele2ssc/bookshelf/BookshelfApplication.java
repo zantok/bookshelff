59f8660f3d330ea021d7080af221e86b6ec83d68
Dimitrijs Fedotovs <dima@fedoto.ws>
2017-10-09 21:23:01 +0300

    initial commit

---
+++ src/main/java/lv/tele2ssc/bookshelf/BookshelfApplication.java
@@ -0,0 +1,12 @@
+package lv.tele2ssc.bookshelf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookshelfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookshelfApplication.class, args);
+	}
+}
